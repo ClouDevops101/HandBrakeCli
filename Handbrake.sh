9aee9f90d713a46db554281983229dbbcfc2abc1
ExpertHack3r75 <heddar.abdelilah@live.fr>
2015-11-13 10:38:04 +0100

    Adding features
    Scanning and compare the output file with the originale file and delete
    the originale video file if everything is good

--- Handbrake.sh
+++ Handbrake.sh
@@ -4,6 +4,7 @@
 # The mp4 format is 480x270_
 # Update  : V0.1.1 25/10/2015 : Adding subtitile, audio Bite rate 128, 
 # Comment : V0.1.2 25/10/2015 : Adding debug 
+# Update
 
 #SRC=/Volumes/Data
 #SRC=/Volumes/Movie
@@ -33,13 +34,26 @@ for FILE in `du -h $SRC/*.$FORMAT_SRC  | sort -n -r  | awk '{print $2}'`
                 sleep 7
 		#echo "-i $FILE -o $DEST/$filename.$DEST_EXT"
 		osascript -e 'display notification "Start compressing"'
-		$HB -i $TMP/$TMP_FILE -o $TMP/$filename.$DEST_EXT  -f av_mp4 -m -a 1,2  -E copy:aac,copy:aac  --audio-fallback aac -B 128 -e x264 -q 20 -s 1,2 -N fre,eng -x $XO
+		$HB -i $TMP/$TMP_FILE -o $TMP/$filename.$DEST_EXT  -f av_mp4 -m -a 1,2  -E copy:aac  --audio-fallback aac -B 128 -e x264 -q 20 -s 1,2 -N fre,eng -x $XO
 		osascript -e 'display notification "End compressing"'
         	Size_SRC=`du -h $FILE`
 		Size_HB=`du -h $TMP/$filename.$DEST_EXT`
 		echo -e "Original size : $Size_SRC \nAfter compres : $Size_HB "
-                mv $TMP/$filename.$DEST_EXT $DEST
 		rm -f $TMP/$TMP_FILE
+		# comparaison de la compression avec HB
+
+		film_ori=`HandBrakeCLI --scan -i $FILE 2>&1  | grep Duration | awk '{print $2}' | tr ',' ' ' | cut -d'.' -f1`
+		film_HB=`HandBrakeCLI --scan -i $TMP/$filename.$DEST_EXT  2>&1  | grep Duration | awk '{print $2}' | tr ',' ' ' | cut -d'.' -f1`
+
+		if [[ "$film_ori" == "$film_HB" ]]
+		then
+			rm -f $FILE
+			echo "Suppression du fichier $TMP/$TMP_FILE"
+		else
+			echo "Fichier non supprime merci de verifier les deux fichiers"
+		fi
+
+                mv $TMP/$filename.$DEST_EXT $DEST
 		osascript -e 'display notification "Sleeping ..."'
         	sleep $WAIT
 	done
